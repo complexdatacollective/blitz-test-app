66fc8870ae63d0e5535c05ac8c2241f1856bdb62
Joshua Melville <joshua.melville@northwestern.edu>
2022-02-25 11:28:02 +0200

    remove current

--- app/core/components/NavBar.tsx
+++ app/core/components/NavBar.tsx
@@ -154,24 +154,18 @@ export default function Example() {
               </div>
             </div>
           </div>
-
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
+                <Link href={item.href} key={item.name}>
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
